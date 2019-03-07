8e990e797a19fec90d84db1fa071ed9100b38e6f
Amelia Paniagua Gálvez <paniagua10@hotmail.com>
2019-03-07 14:31:59 +0100

    Añadido archivo config.js

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ const express  = require('express');
 const mongoose = require('mongoose');
 const morgan   = require('morgan');
 const routes = require('./routes.js');
+const config = require('./config');
 
 const app      = express();
 
-mongoose.connect('mongodb://localhost:27017/merceria', { useNewUrlParser: true })
+mongoose.connect(config.db_uri, { useNewUrlParser: true })
   .then(db   => console.log ('Conexión correcta a la BD'))
   .catch(err => console.log ('Error en la conexión a la BD'));
 
@@ -22,4 +23,4 @@ app.use(express.json());
 app.use ('/api', routes);
 
 // --- PUERTO DE ESCUCHA
-app.listen (3000, () => console.log('Servidor iniciado en puerto 3000'));
+app.listen (config.port, () => console.log('Servidor iniciado en puerto 3000'));
